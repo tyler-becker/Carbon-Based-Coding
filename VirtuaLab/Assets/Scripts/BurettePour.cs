03579095cca1ff0b3256ffc83ce72b1103f4649a
Chris <christopher.mclaughlin@selu.edu>
2015-10-01 11:53:42 -0500

    Added Unity project Virtualab, with first attempt at Titration coding.
    Signed-off-by: Chris <christopher.mclaughlin@selu.edu>

---
+++ VirtuaLab/Assets/Scripts/BurettePour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurettePour : MonoBehaviour
+{
+    public Transform[] liquidPosition;
+    private int currentPosition;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = liquidPosition[0].position;
+        currentPosition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPosition < liquidPosition.Length)
+        {
+            //Transform objects current position to the next position in the liquidPosition array at a specified speed.
+            transform.position = Vector3.MoveTowards(transform.position,
+                liquidPosition[currentPosition].position, moveSpeed * Time.deltaTime);
+
+            if (transform.position == liquidPosition[currentPosition].position)
+            {
+                currentPosition++;
+            }
+        }
+    }
+}
---
+++ VirtuaLab/Assets/Scripts/TitrationManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+liquidPour()            = a method that animates liquid pour (currently done by BurretePour class)
+transferVolume()        = a method that transfers volume dispensed from burette to beaker accurately/visually
+speedPour()             = a method that allows the user to adjust the pour speed
+neutralColorChange()    = a method that changes the color of the liquid in the beaker when it is neutralized
+closeNeutralColor()     = a method that changes the color of the inthe beaker for a frame or two when close to neutral 
+dripPour()              = a method that animates a drop or steady pour from the burette to the beaker
+*/
+
+
+public class TitrationManager : MonoBehaviour
+{
+    public Rect speedRect, upRect, downRect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*if (transform.position = BurettePour.liquidPosition[1])
+        {
+
+        }*/
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(speedRect, "Speed");
+        GUI.Button(upRect, "Up");
+        GUI.Button(downRect, "Down");
+    }
+
+    void liquidPour()
+    {
+
+    }
+    
+    void transferVolume()
+    {
+        //may need to use a seperate script for each object??
+        float obj1radius = transform.position.x;
+        float obj2radius = 0;
+
+        float height1 = 0;
+        //Calculate height2 from height1 -- (pi(r^2)height1)/(pi(R^2)) = height2
+        float height2 = (Mathf.PI * (obj1radius * obj1radius) * height1) / (Mathf.PI * (obj2radius * obj2radius));
+
+        float volume1 = Mathf.PI * (obj1radius * obj1radius) * height1;
+        float volume2 = Mathf.PI * (obj2radius * obj2radius) * height2;
+    }
+
+    void speedPour()
+    {
+
+    }
+
+    void neutralColorChange()
+    {
+
+    }
+
+    void closeNeutralColor()
+    {
+
+    }
+
+    void dripPour()
+    {
+
+    }
+}
