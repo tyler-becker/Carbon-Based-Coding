8559edecb96f7c59f731dd9aca7f071c9924b0f5
Brannon Crochet <brannon.crochet@seluedu>
2015-10-10 16:54:18 -0500

    Added to where you can change from scene to scene and added some coding for a potential inventory system
    Signed-off-by: Brannon Crochet <brannon.crochet@seluedu>

---
+++ VirtuaLab/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	void OnMouseDown() {
+        Application.LoadLevel("ChemicalStorage");
+	}
+}
---
+++ VirtuaLab/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+    public string Name;
+    public int Id;
+    public string Description;
+    public Texture2D Icon;
+
+	
+}
